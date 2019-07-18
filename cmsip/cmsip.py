e74924b78b4fb6a137adf02318dc8045d508e17b
Jin Li <lijin.abc@gmail.com>
2019-07-18 15:08:11 -0500

    add skeleton in cmsip.py

--- cmsip/cmsip.py
+++ cmsip/cmsip.py
@@ -6,6 +6,36 @@ __version__ = "0.0.0.1"
 import argparse
 import yaml
 
+def bsmap(config):
+	print('==>bsmap<==')
+
+def removeCommonReads():
+	print('==>removeCommonReads<==')
+
+def estimateSizeFactors():
+	print('==>estimateSizeFactors<==')
+
+def normalizeTotalWigsum():
+	print('==>normalizeTotalWigsum<==')
+
+def removeDuplication():
+	print('==>removeDuplication<==')
+
+def normalizeWigsum():
+	print('==>normalizeWigsum<==')
+
+def DMR():
+	print('==>DMR<==')
+
+def run(config):
+	bsmap(config)
+	removeCommonReads()
+	estimateSizeFactors()
+	normalizeTotalWigsum()
+	removeDuplication()
+	normalizeWigsum()
+	DMR()
+
 def main():
 	parser = argparse.ArgumentParser(
 		description='CMS-IP sequencing analysis workflow'
@@ -17,6 +47,7 @@ def main():
 		)
 	args = parser.parse_args()
 	config=yaml.load(args.config, Loader=yaml.FullLoader)
+	run(config)
 
 if __name__ == "__main__":
 	main()
