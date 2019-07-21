e29da2a7bdf2ad93503002f9b005d6b843246213
Jin Li <lijin.abc@gmail.com>
2019-07-21 02:47:34 -0500

    add meancovtable

--- README.md
+++ README.md
@@ -10,7 +10,9 @@ Detecting differential 5hmC regions from CMS-IP sequencing data.
 
 `bsmap` is a component in the MOABS package. See more at MOABS ([https://github.com/sunnyisgalaxy/moabs](https://github.com/sunnyisgalaxy/moabs)).
 
-- samtools
+- samtools: [http://samtools.sourceforge.net](http://samtools.sourceforge.net)
 
-- bedtools
+- bedtools: [https://bedtools.readthedocs.io](https://bedtools.readthedocs.io)
+
+- kentUtils: [https://github.com/ENCODE-DCC/kentUtils](https://github.com/ENCODE-DCC/kentUtils)
 
--- cmsip/cmsip.py
+++ cmsip/cmsip.py
@@ -159,7 +159,7 @@ def normalizetwsref(tws, sizefactors, verbose=False):
 
 import matplotlib.pyplot as plt
 def barplot(config, tws):
-	outfile=os.path.join(config['datainfo']['outdir'], 'results', 'twsn_barplot.pdf')
+	outfile=os.path.join(config['datainfo']['outdir'], 'qcstats_twsn_barplot.pdf')
 	plt.figure(figsize=(5, 5))
 	ax=plt.axes()
 	ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, loc: '{:,}'.format(int(x))))
@@ -169,8 +169,7 @@ def barplot(config, tws):
 	plt.savefig(outfile, bbox_inches='tight')
 
 def QCstats(config, qcstats):
-	statfile=os.path.join(config['datainfo']['outdir'], 'results', 'qcstats.txt')
-	runcmd('mkdir -p ' + os.path.dirname(statfile), echo=config['verbose'])
+	statfile=os.path.join(config['datainfo']['outdir'], 'qcstats.txt')
 	with open(statfile, 'w+') as f:
 		print('\t'.join((
 			'sample_id'
@@ -214,27 +213,97 @@ def QCstats(config, qcstats):
 					)
 				)), file=f)
 
-def removeDuplication():
-	print('==>removeDuplication<==')
+def removedupref(config):
+	if config['verbose']:
+		print('==>removedupref<==')
+	indir=os.path.join(config['datainfo']['outdir'], 'removeCommonReads', 'reference')
+	outdir=os.path.join(config['datainfo']['outdir'], 'removedupref')
+	for sampleinfo in config['sampleinfo']:
+			infile=os.path.join(indir, sampleinfo['sampleid'] + '.bam')
+			outfile=os.path.join(outdir, sampleinfo['sampleid'] + '.bam')
+			runcmd('mkdir -p ' + os.path.dirname(outfile), echo=config['verbose'])
+			runcmd('samtools rmdup %s %s' % (infile, outfile), echo=config['verbose'])
 
-def normalizeWigsum():
-	print('==>normalizeWigsum<==')
+import pandas as pd
+def genomecov(config, statfile):
+	if config['verbose']:
+		print('==>genomecov<==')
+	sizefactors=pd.read_csv(statfile, sep='\t', index_col='sample_id', usecols=['sample_id', 'sizefactors']).to_dict()['sizefactors']
+	indir=os.path.join(config['datainfo']['outdir'], 'removedupref')
+	outdir=os.path.join(config['datainfo']['outdir'], 'genomecov')
+	for sampleinfo in config['sampleinfo']:
+			infile=os.path.join(indir, sampleinfo['sampleid'] + '.bam')
+			outfile=os.path.join(outdir, sampleinfo['sampleid'] + '.bedgraph')
+			cmd = "bedtools genomecov -ibam %s -bg -scale %f > %s" % (infile, sizefactors[sampleinfo['sampleid']], outfile)
+			runcmd('mkdir -p ' + os.path.dirname(outfile), echo=config['verbose'])
+			runcmd(cmd, echo=config['verbose'])
 
-def DMR():
-	print('==>DMR<==')
+def genomemeancov(config):
+	if config['verbose']:
+		print('==>genomemeancov<==')
+	indir=os.path.join(config['datainfo']['outdir'], 'genomecov')
+	outdir=os.path.join(config['datainfo']['outdir'], 'genomemeancov')
+	for sampleinfo in config['sampleinfo']:
+			infile=os.path.join(indir, sampleinfo['sampleid'] + '.bedgraph')
+			outfile=os.path.join(outdir, sampleinfo['sampleid'] + '.bedgraph')
+			cmd = "bedtools intersect -a %s -b %s -wo | awk -v OFS='\t' -e '{ print $1, $2, $3, $7*$8/%d }' | bedtools groupby -g 1,2,3 -c 4 -o sum > %s" % (config['datainfo']['windowfile'], infile, config['datainfo']['windowsize'], outfile)
+			runcmd('mkdir -p ' + os.path.dirname(outfile), echo=config['verbose'])
+			if config['verbose']:
+				print(cmd)
+			cp=subprocess.run(cmd, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+			if cp.returncode != 0:
+				print('Error: %s failed.' % cmd, vars(cp), file=sys.stderr)
+				sys.exit(-1)
+
+def meancovtable(config):
+	if config['verbose']:
+		print('==>meancovtable<==')
+	indir=os.path.join(config['datainfo']['outdir'], 'genomemeancov')
+	outfile=os.path.join(config['datainfo']['outdir'], 'meancovtable.txt.gz')
+	sampleids=[sampleinfo['sampleid'] for sampleinfo in config['sampleinfo']]
+	fs=[os.path.join(indir, id+'.bedgraph') for id in sampleids]
+	cmd = "bedtools unionbedg -i %s -header -names %s | awk -v FS='\t' -v OFS='\t' -v separator='-' -v col=3 -e '{ for (i=1; i<col; i++) { printf $i ((i<NF)?separator:ORS) } for (i=col; i<=NF; i++) { printf $i ((i<NF)?OFS:ORS) } }' | gzip -n > %s" % (' '.join(fs), ' '.join(sampleids), outfile)
+	runcmd('mkdir -p ' + os.path.dirname(outfile), echo=config['verbose'])
+	if config['verbose']:
+		print(cmd)
+	cp=subprocess.run(cmd, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	if cp.returncode != 0:
+		print('Error: %s failed.' % cmd, vars(cp), file=sys.stderr)
+		sys.exit(-1)
+
+def DMR(config, cnttablefile):
+	if config['verbose']:
+		print('==>DMR<==')
 
 def run(config):
-	qcstats = {}
-	qcstats['mpstat'] = bsmap(config)
-	qcstats['comm'] = removeCommonReads(config)
-	qcstats['twss'] = totalwigsums(config)
-	qcstats['sizefactors'] = estimateSizeFactors(qcstats['twss']['spikein'], config['verbose'])
-	qcstats['twsrefnorm'] = normalizetwsref(qcstats['twss']['reference'], qcstats['sizefactors'], config['verbose'])
-	QCstats(config, qcstats)
-	barplot(config, qcstats['twsrefnorm'])
-	removeDuplication()
-	normalizeWigsum()
-	DMR()
+	statfile = os.path.join(config['datainfo']['outdir'], 'qcstats.txt')
+	if 'statfile' in config['datainfo']:
+		statfile = config['datainfo']['statfile']
+	if not os.path.exists(statfile):
+		qcstats = {}
+		qcstats['mpstat'] = bsmap(config)
+		qcstats['comm'] = removeCommonReads(config)
+		qcstats['twss'] = totalwigsums(config)
+		qcstats['sizefactors'] = estimateSizeFactors(qcstats['twss']['spikein'], config['verbose'])
+		qcstats['twsrefnorm'] = normalizetwsref(qcstats['twss']['reference'], qcstats['sizefactors'], config['verbose'])
+		QCstats(config, qcstats)
+		barplot(config, qcstats['twsrefnorm'])
+		
+	cnttablefile=os.path.join(config['datainfo']['outdir'], 'meancovtable.txt.gz')
+	if 'cnttablefile' in config['datainfo']:
+		cnttablefile = config['datainfo']['cnttablefile']
+	if not os.path.exists(cnttablefile):
+		if not os.path.exists(config['datainfo']['windowfile']):
+			print('Error: the window file should be specified in the configuration file.'
+					, 'Window file can be generated using bedtools makewindows.'
+					, '  e.g. bedtools makewindows -g <(fetchChromSizes hg38) -w 100 > hg38_w100.bed'
+					, sep='\n')
+			sys.exit(-1)
+		removedupref(config)
+		genomecov(config, statfile)
+		genomemeancov(config)
+		meancovtable(config)
+	DMR(config, cnttablefile)
 
 def main():
 	parser = argparse.ArgumentParser(
