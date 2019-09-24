afc7e1311e1420e7a1f286c3c29673868a936971
Jin Li <lijin.abc@gmail.com>
2019-09-23 20:57:03 -0500

    add cms.yaml as template

--- tests/main.sh
+++ tests/main.sh
@@ -2,4 +2,4 @@
 # vim: set noexpandtab tabstop=2:
 
 set -v
-../cmsip/cmsip.py -c cms.yaml
+../cmsip/cmsip.py -c cms.yaml -D genomescaninfo.readscount=True -D genomescaninfo.fragsize=200 -D dhmrinfo.qthr=0.05 -D resultdir=out_result groupinfo.group1=K groupinfo.group2=W
