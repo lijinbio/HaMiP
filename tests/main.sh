0677ae4d478566df23d9a55a71c553d1dabd76b9
Jin Li <lijin.abc@gmail.com>
2019-10-26 19:08:14 -0500

    rm tests

--- tests/main.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# vim: set noexpandtab tabstop=2:
-
-set -v
-../cmsip/cmsip.py -c cms.yaml -D genomescaninfo.readscount=True -D genomescaninfo.fragsize=200 -D dhmrinfo.qthr=0.05 -D resultdir=out_result groupinfo.group1=K groupinfo.group2=W
